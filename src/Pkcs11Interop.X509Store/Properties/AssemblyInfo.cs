3522c29228f35acf5b903f0e9182c5dfd6c6eda3
Jaroslav Imrich <jimrich@jimrich.sk>
2017-10-22 14:54:09 +0200

    Solution created in VS2017

---
+++ src/Pkcs11Interop.X509Store/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pkcs11Interop.X509Store")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop.X509Store")]
+[assembly: AssemblyCopyright("Copyright 2017 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e5fb0c6d-e99a-4160-9f55-ce13ea7d131f")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
