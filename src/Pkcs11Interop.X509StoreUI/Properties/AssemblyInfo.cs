dd7165540afb41e2481ff71bc389f5405c205daa
Jaroslav Imrich <jimrich@jimrich.sk>
2017-10-22 15:21:05 +0200

    Added Pkcs11Interop.X509StoreUI project

---
+++ src/Pkcs11Interop.X509StoreUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pkcs11Interop.X509StoreUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop.X509StoreUI")]
+[assembly: AssemblyCopyright("Copyright 2017 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f1064fbc-6c7e-4d6a-8a0a-1442b466f898")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
