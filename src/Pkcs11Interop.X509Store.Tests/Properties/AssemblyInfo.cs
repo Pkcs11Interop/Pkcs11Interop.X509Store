02dab086f5e9b33b905f631a922fffaaf6103fd9
Jaroslav Imrich <jimrich@jimrich.sk>
2017-10-22 15:58:32 +0200

    Added Pkcs11Interop.X509Store.Tests project

---
+++ src/Pkcs11Interop.X509Store.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pkcs11Interop.X509Store.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop.X509Store")]
+[assembly: AssemblyCopyright("Copyright 2017 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bbe56bba-ee94-4d09-9c77-062a26950022")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
--- src/Pkcs11Interop.X509Store/Pkcs11X509Store.cs
+++ src/Pkcs11Interop.X509Store/Pkcs11X509Store.cs
@@ -20,10 +20,78 @@
  */
 
 using System;
+using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
 
 namespace Net.Pkcs11Interop.X509Store
 {
-    public class Pkcs11X509Store
+    public class Pkcs11X509Store : IDisposable
     {
+        private bool _disposed = false;
+
+        private Pkcs11 _pkcs11 = null;
+
+        public Pkcs11X509Store(string libraryPath)
+        {
+            if (string.IsNullOrEmpty(libraryPath))
+                throw new ArgumentNullException(nameof(libraryPath));
+
+            try
+            {
+                _pkcs11 = new Pkcs11(libraryPath, AppType.MultiThreaded);
+            }
+            catch (Pkcs11Exception ex)
+            {
+                if (ex.RV == CKR.CKR_CANT_LOCK)
+                    _pkcs11 = new Pkcs11(libraryPath, AppType.SingleThreaded);
+                else
+                    throw;
+            }
+        }
+
+        #region IDisposable
+
+        /// <summary>
+        /// Disposes object
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes object
+        /// </summary>
+        /// <param name="disposing">Flag indicating whether managed resources should be disposed</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                // Dispose managed objects
+                if (disposing)
+                {
+                    if (_pkcs11 != null)
+                    {
+                        _pkcs11.Dispose();
+                        _pkcs11 = null;
+                    }
+                }
+
+                // Dispose unmanaged objects
+
+                _disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Class destructor that disposes object if caller forgot to do so
+        /// </summary>
+        ~Pkcs11X509Store()
+        {
+            Dispose(false);
+        }
+
+        #endregion
     }
 }
