e1c540c59e7be714b8f9be518fb9806e0ef6660d
Jaroslav Imrich <jimrich@jimrich.sk>
2024-04-20 22:42:42 +0200

    Updated year in copyright

--- src/Pkcs11Interop.X509Store.Tests/SignedCmsTest.cs
+++ src/Pkcs11Interop.X509Store.Tests/SignedCmsTest.cs
@@ -1,4 +1,25 @@
-﻿using System.Security.Cryptography;
+﻿/*
+ *  Copyright 2017-2024 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+using System.Security.Cryptography;
 using System.Security.Cryptography.Pkcs;
 using System.Text;
 using Net.Pkcs11Interop.X509Store.Tests.SoftHsm2;
