c7cc3a11d8d3744ad8eaafd51a36ebf663375085
Jaroslav Imrich <jimrich@jimrich.sk>
2024-05-01 12:50:13 +0200

    #38 Added PrivateKeyObjectNotFoundException and PublicKeyObjectNotFoundException

---
+++ src/Pkcs11Interop.X509Store.Tests/SoftHsm2/CancellingPinProvider.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *  Copyright 2017-2024 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+namespace Net.Pkcs11Interop.X509Store.Tests.SoftHsm2
+{
+    public class CancellingPinProvider : IPinProvider
+    {
+        public GetPinResult GetTokenPin(Pkcs11X509StoreInfo storeInfo, Pkcs11SlotInfo slotInfo, Pkcs11TokenInfo tokenInfo)
+        {
+            return new GetPinResult(true, null);
+        }
+
+        public GetPinResult GetKeyPin(Pkcs11X509StoreInfo storeInfo, Pkcs11SlotInfo slotInfo, Pkcs11TokenInfo tokenInfo, Pkcs11X509CertificateInfo certificateInfo)
+        {
+            return new GetPinResult(true, null);
+        }
+    }
+}
---
+++ src/Pkcs11Interop.X509Store/PrivateKeyObjectNotFoundException.cs
@@ -0,0 +1,54 @@
+﻿/*
+ *  Copyright 2017-2024 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Net.Pkcs11Interop.X509Store
+{
+    /// <summary>
+    /// Exception indicating that private key object corresponding to certificate object was found on token
+    /// </summary>
+    [Serializable]
+    public class PrivateKeyObjectNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initializes new instance of PrivateKeyObjectNotFoundException class
+        /// </summary>
+        /// <param name="message">Message that describes the error</param>
+        public PrivateKeyObjectNotFoundException(string message = "The private key object corresponding to the certificate object was not found on the token")
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes new instance of PrivateKeyObjectNotFoundException class
+        /// </summary>
+        /// <param name="info">SerializationInfo that holds the serialized object data about the exception being thrown</param>
+        /// <param name="context">StreamingContext that contains contextual information about the source or destination</param>
+        protected PrivateKeyObjectNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+    }
+}
